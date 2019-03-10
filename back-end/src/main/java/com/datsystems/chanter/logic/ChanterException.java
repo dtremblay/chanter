50016812243206640a7bbf2f10e209640f767488
Daniel Tremblay <daniel.tremblay@gmail.com>
2019-03-10 13:16:31 -0300

    New ChanterException class.

---
+++ back-end/src/main/java/com/datsystems/chanter/logic/ChanterException.java
@@ -0,0 +1,12 @@
+package com.datsystems.chanter.logic;
+
+public class ChanterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4171025318057499636L;
+	public ChanterException(String message) {
+		super(message);
+	}
+}
