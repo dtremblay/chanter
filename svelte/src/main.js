8cbfe1338f558cdd7dc15040253ab5a59a2805a9
Daniel Tremblay <daniel.tremblay@gmail.com>
2023-02-26 18:04:46 -0400

    Updated the svelte script for Vite.

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,9 +1,10 @@
+//import './app.css'
 import App from './App.svelte';
 import { version } from '../package.json';
 
 const app = new App({
-	target: document.body,
-	props: {
+  target: document.getElementById('app'),
+  props: {
 		version: version,
 		baseServer: "http://localhost:8001/chanter",
 		path: window.location
